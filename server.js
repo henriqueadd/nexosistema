ed94ef210221dcf01d45cb563e5c65803257a3c8
henriqueadd <henriqueadriane701@gmail.com>
2026-09-11 10:35:17 -0300

    Initial commit - Nexo Sistemas landing page

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8989;
+const PUBLIC_DIR = __dirname;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(PUBLIC_DIR, req.url === '/' ? 'index.html' : req.url);
+  let extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.mp4':
+      contentType = 'video/mp4';
+      break;
+    case '.mp3':
+      contentType = 'audio/mpeg';
+      break;
+  }
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile(path.join(PUBLIC_DIR, 'index.html'), (err, content) => {
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Server error: ' + error.code);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': `${contentType}; charset=utf-8` });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
